80c12b6386c13f5762415df6e709f56b7c03de8a
Eric Nerby <ericnerby@msn.com>
2020-10-01 10:08:42 -0400

    add some docstrings and begin integrating segments into ship class with some basic methods

--- ships.py
+++ ships.py
@@ -1,89 +1,139 @@
 class Ship():
-    def __init__(self, owner, orientation='h'):
+    """A Ship object represents one ship on the board, along with its
+    segments and string representation for display when necessary."""
+    def __init__(self, owner, ship_type, horizontal_string_reps=[],
+                 vertical_string_reps=[], orientation='h'):
+        self.owner = owner
+        self.ship_type = ship_type
+        self.horizontal_string_reps = horizontal_string_reps
+        self.vertical_string_reps = vertical_string_reps
         self.orientation = orientation
         self.segments = []
-        self.owner = owner
         self.sunk = False
+        self._assign_segments()
     
     def rotate(self):
         if self.orientation == 'h':
             self.orientation = 'v'
+            for segment, segment_rep in zip(self.segments,
+                                            self.vertical_string_reps):
+                segment.string_rep_tup = segment_rep
         elif self.orientation == 'v':
             self.orientation = 'h'
+            for segment, segment_rep in zip(self.segments,
+                                            self.horizontal_string_reps):
+                segment.string_rep_tup = segment_rep
+    
+    def _assign_segments(self):
+        if self.orientation == 'h':
+            for segment in self.horizontal_string_reps:
+                self.segments.append(Segment(segment, self))
+        elif self.orientation == 'v':
+            for segment in self.vertical_string_reps:
+                self.segments.append(Segment(segment, self))
+    
+    def __len__(self):
+        return len(self.segments)
 
 
 class Carrier(Ship):
-    HORIZONTAL_STRING_REPS = [
-        ("[=", "[x"),
-        ("==", "=x"),
-        ("#=", "#x"),
-        ("==", "=x"),
-        ("=]", "x]"),
-    ]
-    VERTICAL_STRING_REPS = [
-        ("[]", "[X"),
-        ("||", "|X"),
-        ("#|", "#X"),
-        ("||", "|X"),
-        ("[]", "[X"),
-    ]
+    def __init__(self, owner, orientation='h'):
+        HORIZONTAL_STRING_REPS = [
+            ("[=", "[x"),
+            ("==", "=x"),
+            ("#=", "#x"),
+            ("==", "=x"),
+            ("=]", "x]"),
+        ]
+        VERTICAL_STRING_REPS = [
+            ("[]", "[X"),
+            ("||", "|X"),
+            ("#|", "#X"),
+            ("||", "|X"),
+            ("[]", "[X"),
+        ]
+        super().__init__(owner, 'Carrier', HORIZONTAL_STRING_REPS,
+                         VERTICAL_STRING_REPS, orientation)
 
 
 class Battleship(Ship):
-    HORIZONTAL_STRING_REPS = [
-        ("<=", "<x"),
-        ("==", "=x"),
-        ("==", "=x"),
-        ("=]", "x]"),
-    ]
-    VERTICAL_STRING_REPS = [
-        ("/\\", "/X"),
-        ("||", "|X"),
-        ("||", "|X"),
-        ("[]", "[X"),
-    ]
+    def __init__(self, owner, orientation='h'):
+        HORIZONTAL_STRING_REPS = [
+            ("<=", "<x"),
+            ("==", "=x"),
+            ("==", "=x"),
+            ("=]", "x]"),
+        ]
+        VERTICAL_STRING_REPS = [
+            ("/\\", "/X"),
+            ("||", "|X"),
+            ("||", "|X"),
+            ("[]", "[X"),
+        ]
+        super().__init__(owner, 'Battleship', HORIZONTAL_STRING_REPS,
+                         VERTICAL_STRING_REPS, orientation)
+
 
 
 class Destroyer(Ship):
-    HORIZONTAL_STRING_REPS = [
-        ("<=", "<x"),
-        ("==", "=x"),
-        ("=]", "x]"),
-    ]
-    VERTICAL_STRING_REPS = [
-        ("/\\", "/X"),
-        ("||", "|X"),
-        ("[]", "[X"),
-    ]
+    def __init__(self, owner, orientation='h'):
+        HORIZONTAL_STRING_REPS = [
+            ("<=", "<x"),
+            ("==", "=x"),
+            ("=]", "x]"),
+        ]
+        VERTICAL_STRING_REPS = [
+            ("/\\", "/X"),
+            ("||", "|X"),
+            ("[]", "[X"),
+        ]
+        super().__init__(owner, 'Destroyer', HORIZONTAL_STRING_REPS,
+                         VERTICAL_STRING_REPS, orientation)
 
 
 class Submarine(Ship):
-    HORIZONTAL_STRING_REPS = [
-        ("<=", "<x"),
-        ("^=", "^x"),
-        ("=>", "x>"),
-    ]
-    VERTICAL_STRING_REPS = [
-        ("/\\", "/X"),
-        ("|>", "|X"),
-        ("\\/", "\\X"),
-    ]
+    def __init__(self, owner, orientation='h'):
+        HORIZONTAL_STRING_REPS = [
+            ("<=", "<x"),
+            ("^=", "^x"),
+            ("=>", "x>"),
+        ]
+        VERTICAL_STRING_REPS = [
+            ("/\\", "/X"),
+            ("|>", "|X"),
+            ("\\/", "\\X"),
+        ]
+        super().__init__(owner, 'Submarine', HORIZONTAL_STRING_REPS,
+                         VERTICAL_STRING_REPS, orientation)
 
 
 class PTBoat(Ship):
-    HORIZONTAL_STRING_REPS = [
-        ("<=", "<x"),
-        ("=]", "x]"),
-    ]
-    VERTICAL_STRING_REPS = [
-        ("/\\", "/X"),
-        ("[]", "[X"),
-    ]
+    def __init__(self, owner, orientation='h'):
+        HORIZONTAL_STRING_REPS = [
+            ("<=", "<x"),
+            ("=]", "x]"),
+        ]
+        VERTICAL_STRING_REPS = [
+            ("/\\", "/X"),
+            ("[]", "[X"),
+        ]
+        super().__init__(owner, 'PT Boat', HORIZONTAL_STRING_REPS,
+                         VERTICAL_STRING_REPS, orientation)
 
 
 class Segment():
+    """Each Segment has a location and a string
+    representation and belongs to a certain ship"""
     def __init__(self, string_rep_tup, ship, location=None):
         self.string_rep_tup = string_rep_tup
         self.ship = ship
         self.location = location
         self.hit = False
+    
+    """Return the appropriate ASCII representation of
+    the segment based on whether or not it's been hit."""
+    def __str__(self):
+        if self.hit:
+            return self.string_rep_tup[1]
+        else:
+            return self.string_rep_tup[0]
