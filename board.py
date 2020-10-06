69bfb371bb8c4db100dff860a4a5899c10e7af2a
Eric Nerby <ericnerby@msn.com>
2020-10-06 08:16:57 -0400

    added module docstring to board.py

--- board.py
+++ board.py
@@ -1,3 +1,22 @@
+"""
+Contains the Board and Space classes to place or guess ship locations.
+
+This module is used to set up two 10x10 grids where the computer
+opponent can track guesses on its turn and also place its own ships to
+receive guesses from the player.
+
+The Board is an ordered dictionary such that a space can be indentified
+by letter and number.  For example, board_object['c'][7] would
+represent 'C7' on the grid.
+
+Classes
+-------
+Board
+    An OrderedDict representing a grid with the letters A-J and numbers 1-10
+Space
+    A class for a space on the board which can hold a ship segment.
+"""
+
 from collections import OrderedDict
 from string import ascii_lowercase as alphabet
 
