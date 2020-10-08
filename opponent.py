0706d21d8864a308be090ff1ffa2bb1be6f5e6e7
Eric Nerby <ericnerby@msn.com>
2020-10-08 19:09:03 -0400

    added check_spaces method on opponent

--- opponent.py
+++ opponent.py
@@ -22,6 +22,30 @@ class Opponent:
             rotate = random.randint(0, 1)
             if rotate:
                 ship.rotate()
+            if self._check_spaces(letter, number, ship):
+                # assign segments to spaces
+                pass
 
-    def _check_spaces(self, starting_space, length, orientation):
-        pass
+    def _check_spaces(self, letter, number, ship):
+        """Check if spaces are available at given starting space for ship."""
+        if ship.orientation == 'h':  # if ship is horizontal
+            for index, _ in enumerate(ship.segments, 0):
+                # If any proposed space for a ship does not exist
+                #   or is already occupied, stop the for loop and
+                #   return False from the method.
+                if (number + index >= len(self.field_board[letter])
+                    or self.field_board[letter][number + index].segment):
+                    return False
+        else:  # if ship is vertical
+            letter_index = Opponent.row_letters.index(letter)
+            for index, _ in enumerate(ship.segments, 0):
+                # If any proposed space for a ship does not exist
+                #   or is already occupied, stop the for loop and
+                #   return False from the method.
+                try:
+                    loop_letter = Opponent.row_letters[letter_index + index]
+                except:
+                    return False
+                if self.field_board[loop_letter][number].segment:
+                    return False
+        return True
