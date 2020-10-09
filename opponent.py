ddcac3592391206fc27b126decd535d11746bada
Eric Nerby <ericnerby@msn.com>
2020-10-08 20:13:06 -0400

    building place_ships method on Opponent class. still not working.

--- opponent.py
+++ opponent.py
@@ -1,51 +1,54 @@
 import random
-from string import ascii_lowercase as alphabet
 
 from board import Board
 from fleet import Fleet
 
 
 class Opponent:
-    row_letters = alphabet[:10]
-
     def __init__(self):
         self.radar_board = Board('radar')
         self.radar_fleet = Fleet()
 
         self.field_board = Board('field')
         self.field_fleet = Fleet()
+        self.place_ships()
 
     def place_ships(self):
         for ship in self.field_fleet:
-            letter = random.choice(Opponent.row_letters)
-            number = random.randint(1, 11)
-            rotate = random.randint(0, 1)
-            if rotate:
-                ship.rotate()
-            if self._check_spaces(letter, number, ship):
-                # assign segments to spaces
-                pass
+            self._place_ship(ship)
 
-    def _check_spaces(self, letter, number, ship):
+    def _place_ship(self, ship):
+        row = random.randint(0, 10)
+        column = random.randint(0, 10)
+        rotate = random.randint(0, 1)
+        if rotate:
+            ship.rotate()
+        if self._check_spaces(row, column, ship):
+            if ship.orientation == 'h':
+                for index, segment in enumerate(ship.segments):
+                    self.field_board[row][column + index].segment = segment
+            if ship.orientation == 'v':
+                for index, segment in enumerate(ship.segments):
+                    self.field_board[row + index][column].segment = segment
+        else:
+            self._place_ship(ship)
+
+    def _check_spaces(self, row, column, ship):
         """Check if spaces are available at given starting space for ship."""
+        import pdb; pdb.set_trace()
         if ship.orientation == 'h':  # if ship is horizontal
-            for index, _ in enumerate(ship.segments, 0):
+            for index in range(len(ship)):
                 # If any proposed space for a ship does not exist
                 #   or is already occupied, stop the for loop and
                 #   return False from the method.
-                if (number + index >= len(self.field_board[letter])
-                    or self.field_board[letter][number + index].segment):
+                if column + index >= len(self.field_board[row]):
+                    return False
+                elif self.field_board[row][column + index].segment:
                     return False
         else:  # if ship is vertical
-            letter_index = Opponent.row_letters.index(letter)
-            for index, _ in enumerate(ship.segments, 0):
-                # If any proposed space for a ship does not exist
-                #   or is already occupied, stop the for loop and
-                #   return False from the method.
-                try:
-                    loop_letter = Opponent.row_letters[letter_index + index]
-                except:
+            for index in range(len(ship)):
+                if row + index >= len(self.field_board):
                     return False
-                if self.field_board[loop_letter][number].segment:
+                elif self.field_board[row + index][column].segment:
                     return False
         return True
