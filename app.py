429e11281fa03cf751d7fc47e0479d5d2dfd963e
Eric Nerby <ericnerby@msn.com>
2020-10-21 12:17:10 -0400

    add help menu

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import time
 from gameconversions import convert_from_index, convert_to_index
 from opponent import Opponent
 
+WAIT_TIME = 3
 
 WELCOME_SCREEN = r"""
        . |_
@@ -25,6 +26,28 @@ and have fun!
 
 """
 
+HELP_STRING = """
+HELP
+----
+
+To quit at anytime, type '-q' and hit Enter.
+To bring up this menu, enter '-h'.
+
+When it is your turn, enter your guess as a letter followed by a number
+with no spaces or characters in between (eg 'a1'or 'B10') and hit Enter
+to submit your guess.  The computer's response to your guess will
+display for {} seconds.  A space that you have already guessed will
+not be accepted.
+
+When it is the computer's turn, respond to the computer's guess with
+'h' for hit or 'm' for miss.  The default will be miss if you don't
+enter a recognized command.  If a sunken ship is possible, you'll
+receive an additional prompt with a numbered list of possible ships.
+If there is no sunken ship at that time, you can simply hit Enter
+without a number or type '0'.
+""".format(WAIT_TIME)
+
+
 # --------- Helper Functions --------- #
 class Player:
     """Create a Player for the game."""
@@ -41,8 +64,8 @@ def clear():
 
 
 def sleeper():
-    """Pause code execution for three seconds."""
-    time.sleep(3)
+    """Pause code execution for seconds stored in WAIT_TIME."""
+    time.sleep(WAIT_TIME)
 
 
 def check_help_and_quit(user_input):
@@ -50,8 +73,10 @@ def check_help_and_quit(user_input):
     if re.match(r'-q', user_input):
         sys.exit()
     elif re.match(r'-h', user_input):
-        # bring up help screen
-        pass
+        print(HELP_STRING)
+        _ = input("Hit Enter to return to game.")
+        return True
+    return False
 
 
 def display_field():
@@ -74,7 +99,9 @@ def player_turn():
     """Take player's guess, mark it, and provide feedback."""
     clear()
     player_input = input("Please enter your guess in the format 'A1': ")
-    check_help_and_quit(player_input)
+    if check_help_and_quit(player_input):
+        player_turn()
+        return
     player_guess = re.match(r'([a-zA-Z])(\d+)', player_input)
     if player_guess:
         row_guess, column_guess = player_guess.group(1, 2)
@@ -85,16 +112,16 @@ def player_turn():
             print("Your guess was outside of the range of the board.")
             sleeper()
             player_turn()
+            return
+        segment = opponent.field_board[row_guess][column_guess].take_guess()
+        if segment:
+            print("'{}' is a hit!".format(player_input))
+            ship = segment.ship
+            if ship.sunk:
+                print("You sunk my {}!".format(ship))
         else:
-            segment = opponent.field_board[row_guess][column_guess].take_guess()
-            if segment:
-                print("'{}' is a hit!".format(player_input))
-                ship = segment.ship
-                if ship.sunk:
-                    print("You sunk my {}!".format(ship))
-            else:
-                print("'{}' is a miss!".format(player_input))
-            sleeper()
+            print("'{}' is a miss!".format(player_input))
+        sleeper()
     else:
         print(
            "Please make sure you're entering your guess in the format 'A1'.")
@@ -102,16 +129,21 @@ def player_turn():
         player_turn()
 
 
-def opponent_turn():
+def opponent_turn(existing_row=None, existing_column=None):
     """Make guess, prompt player, and mark guess."""
     clear()
-    row_guess, column_guess = opponent.make_guess()
+    if existing_row == None and existing_column == None:
+        row_guess, column_guess = opponent.make_guess()
+    else:
+        row_guess = existing_row
+        column_guess = existing_column
     row_friendly = convert_from_index(row_guess, 'upper')
     column_friendly = convert_from_index(column_guess, 'one')
     print("I'm going to guess... {}{}.".format(
         row_friendly, column_friendly))
     player_input = input("'h' for hit, 'm' for miss. [M/h] ")
-    check_help_and_quit(player_input)
+    if check_help_and_quit(player_input):
+        opponent_turn(row_guess, column_guess)
     if re.match(r'h', player_input, re.I):
         opponent.take_guess_answer(row_guess, column_guess, True)
     else:
